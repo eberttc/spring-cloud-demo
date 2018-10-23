283b037949d79e612d413ebcd0c9e402dec3579c
ebert toribio cervantes <ebercito1988@hotmail.com>
2018-10-23 00:17:08 -0500

    change config uri

--- customer-api/src/main/java/com/learning/microservices/service/CustomerService.java
+++ customer-api/src/main/java/com/learning/microservices/service/CustomerService.java
@@ -2,7 +2,10 @@ package com.learning.microservices.service;
 
 import com.learning.microservices.interfaces.Customer;
 import com.learning.microservices.repository.CustomerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,8 +14,13 @@ import java.util.stream.Collectors;
 @Service
 public class CustomerService {
 
+	Logger log= LoggerFactory.getLogger(this.getClass());
+
 	@Autowired
 	private CustomerRepository customerRepository;
+
+	@Value("${customer.api.list.currency}")
+	private List<String> currencies;
 
 	public List<Customer> findAll(){
 		return customerRepository.findAll()
@@ -30,6 +38,7 @@ public class CustomerService {
 	public Customer findById(String id){
 		com.learning.microservices.domain.Customer customerById = customerRepository.findCustomerById(Long.valueOf(id));
 		Customer c=new Customer(customerById.getId().toString(),customerById.getFirstName(),customerById.getLastName(),customerById.getAge());
+		log.info("Currencies:{},{}",currencies.get(0),currencies.get(1));
 		return c;
 	}
 }
